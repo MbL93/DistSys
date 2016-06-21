1d12d203667bd6caf72145fda0ecb0cfef4ae664
MbL93 <marcblickle@gmx.de>
2016-06-21 06:38:39 -0700

    SignalHandler added

--- pe/tinyweb/src/tinyweb.c
+++ pe/tinyweb/src/tinyweb.c
@@ -51,21 +51,34 @@ static volatile sig_atomic_t server_running = false;
 //
 // TODO: Include your function header here
 //
+
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 static void
 sig_handler(int sig)
 {
     switch(sig) {
-        case SIGINT:
-            // use our own thread-safe implemention of printf
-            safe_printf("\n[%d] Server terminated due to keyboard interrupt\n", getpid());
-            server_running = false;
-            break;
-        // TODO: Complete signal handler
-        default:
-            break;
+    case SIGINT:
+                // use our own thread-safe implemention of printf
+                safe_printf("\n[%d] Server terminated due to keyboard interrupt\n", getpid());
+                server_running = false;
+                break;
+            case SIGCHLD:
+                while (waitpid(-1, &status, WNOHANG) > 0) {
+                }
+                break;
+            case SIGABRT:
+                            safe_printf("\n[%d] Server terminated due to system abort\n", getpid());
+                            server_running = false;
+                            break;
+            case SIGSEGV:
+                safe_printf("\n[%d] Server terminated due to segmentation fault\n", getpid());
+                server_running = false;
+                break;
+            default:
+                break;
     } /* end switch */
 } /* end of sig_handler */
-
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
 //
 // TODO: Include your function header here
@@ -73,8 +86,10 @@ sig_handler(int sig)
 static void
 print_usage(const char *progname)
 {
-  fprintf(stderr, "Usage: %s options\n", progname);
-  // TODO: Print the program options
+  fprintf(stderr, "Usage: %s options\n", progname,
+		  "\t-d\tdefines the directory of web-files being loaded\n",
+          "\t-f\tdefines logfile-destination(logging into stdout, if option not set\n",
+          "\t-p\tdefines the port\n");
 } /* end of print_usage */
 
 
@@ -82,22 +97,21 @@ print_usage(const char *progname)
 // TODO: Include your function header here
 //
 static int
-get_options(int argc, char *argv[], prog_options_t *opt)
-{
-    int                 c;
-    int                 err;
-    int                 success = 1;
-    char               *p;
-    struct addrinfo     hints;
+get_options(int argc, char *argv[], prog_options_t *opt) {
+    int c;
+    int err;
+    int success = 1;
+    char *p;
+    struct addrinfo hints;
 
     p = strrchr(argv[0], '/');
-    if(p) {
+    if (p) {
         p++;
     } else {
         p = argv[0];
     } /* end if */
 
-    opt->progname = (char *)malloc(strlen(p) + 1);
+    opt->progname = (char *) malloc(strlen(p) + 1);
     if (opt->progname != NULL) {
         strcpy(opt->progname, p);
     } else {
@@ -106,34 +120,34 @@ get_options(int argc, char *argv[], prog_options_t *opt)
     } /* end if */
 
     opt->log_filename = NULL;
-    opt->root_dir     = NULL;
-    opt->server_addr  = NULL;
-    opt->verbose      =    0;
-    opt->timeout      =  120;
+    opt->root_dir = NULL;
+    opt->server_addr = NULL;
+    opt->verbose = 0;
+    opt->timeout = 120;
 
-    memset(&hints, 0, sizeof(struct addrinfo));
+    memset(&hints, 0, sizeof (struct addrinfo));
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_family = AF_UNSPEC;   /* Allows IPv4 or IPv6 */
+    hints.ai_family = AF_UNSPEC; /* Allows IPv4 or IPv6 */
     hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
 
     while (success) {
         int option_index = 0;
         static struct option long_options[] = {
-            { "file",    required_argument, 0, 0 },
-            { "port",    required_argument, 0, 0 },
-            { "dir",     required_argument, 0, 0 },
-            { "verbose", no_argument,       0, 0 },
-            { "debug",   no_argument,       0, 0 },
-            { NULL,      0, 0, 0 }
+            { "file", required_argument, 0, 0},
+            { "port", required_argument, 0, 0},
+            { "dir", required_argument, 0, 0},
+            { "verbose", no_argument, 0, 0},
+            { "debug", no_argument, 0, 0},
+            { NULL, 0, 0, 0}
         };
 
-        c = getopt_long(argc, argv, "f:p:d:v", long_options, &option_index);
+        c = getopt_long(argc, argv, "f:p:d:hv", long_options, &option_index);
         if (c == -1) break;
 
-        switch(c) {
+        switch (c) {
             case 'f':
                 // 'optarg' contains file name
-                opt->log_filename = (char *)malloc(strlen(optarg) + 1);
+                opt->log_filename = (char *) malloc(strlen(optarg) + 1);
                 if (opt->log_filename != NULL) {
                     strcpy(opt->log_filename, optarg);
                 } else {
@@ -143,20 +157,23 @@ get_options(int argc, char *argv[], prog_options_t *opt)
                 break;
             case 'p':
                 // 'optarg' contains port number
-                if((err = getaddrinfo(NULL, optarg, &hints, &opt->server_addr)) != 0) {
+                if ((err = getaddrinfo(NULL, optarg, &hints, &opt->server_addr)) != 0) {
                     fprintf(stderr, "Cannot resolve service '%s': %s\n", optarg, gai_strerror(err));
                     return EXIT_FAILURE;
                 } /* end if */
+                opt->server_port = (int) ntohs(((struct sockaddr_in*) opt->server_addr->ai_addr)->sin_port);
                 break;
             case 'd':
                 // 'optarg contains root directory */
-                opt->root_dir = (char *)malloc(strlen(optarg) + 1);
+                opt->root_dir = (char *) malloc(strlen(optarg) + 1);
                 if (opt->root_dir != NULL) {
                     strcpy(opt->root_dir, optarg);
                 } else {
                     err_print("cannot allocate memory");
                     return EXIT_FAILURE;
                 } /* end if */
+                break;
+            case 'h':
                 break;
             case 'v':
                 opt->verbose = 1;
@@ -174,7 +191,6 @@ get_options(int argc, char *argv[], prog_options_t *opt)
 
     return success;
 } /* end of get_options */
-
 
 static void
 open_logfile(prog_options_t *opt)
